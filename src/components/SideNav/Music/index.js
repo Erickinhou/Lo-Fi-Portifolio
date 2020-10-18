de57ffc489c0f897303757b66be64a292eee6705
Erickinhou <willianerick17@gmail.com>
2020-10-18 01:14:59 -0300

    solved audio problem

--- src/components/SideNav/Music/index.js
+++ src/components/SideNav/Music/index.js
@@ -1,28 +1,36 @@
-import React, { useState } from 'react';
-import { Volume2, Volume } from 'react-feather';
+import React, { useState, useRef } from 'react';
+import { Volume2, Volume, Play, Pause } from 'react-feather';
 import lofiBokuNoHero from '../../../assets/my hero academia the day lofi.mp3';
+import { MusicArea } from './style';
 
 export default function Music(props) {
-  const [mute, setMute] = useState(false);
-  const HandlePlay = (event) => {
-    const audio = event.target;
-    audio.volume = 0.3;
-    audio.play();
-    console.log('teste');
+  const [muted, setMuted] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(true);
+  const music = useRef(null);
+  const renderVideo = () => (
+    <audio
+      {...(muted ? { muted: true } : {})}
+      onCanPlay={(e) => setIsPlaying(e.target.paused)}
+      autoPlay={false}
+      ref={music}
+    >
+      <source src={lofiBokuNoHero} type="audio/mpeg"></source>
+    </audio>
+  );
+
+  const handlePlay = () => {
+    isPlaying ? music.current.play() : music.current.pause();
+    setIsPlaying(!isPlaying);
   };
   return (
-    <div onClick={() => setMute(!mute)}>
-      {mute ? (
-        <Volume {...props} color="var(--light)"></Volume>
-      ) : (
-        <Volume2 {...props} color="var(--light)"></Volume2>
-      )}
-      <audio
-        {...(mute ? { muted: 'muted' } : {})}
-        onCanPlayCapture={HandlePlay}
-      >
-        <source src={lofiBokuNoHero} type="audio/mpeg"></source>
-      </audio>
-    </div>
+    <MusicArea>
+      <div onClick={handlePlay}>
+        {isPlaying ? <Play {...props}></Play> : <Pause {...props}></Pause>}
+      </div>
+      <div onClick={() => setMuted(!muted)}>
+        {muted ? <Volume {...props}></Volume> : <Volume2 {...props}></Volume2>}
+        {renderVideo()}
+      </div>
+    </MusicArea>
   );
 }
--- src/components/SideNav/Music/style.js
+++ src/components/SideNav/Music/style.js
@@ -1,1 +1,9 @@
 import styled from 'styled-components';
+
+const MusicArea = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-evenly;
+`;
+
+export { MusicArea };
