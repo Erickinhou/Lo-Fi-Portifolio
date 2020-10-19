bc70db1dece02b2af53f30fa924eecb5f37fb458
Erickinhou <willianerick17@gmail.com>
2020-10-18 22:01:02 -0300

    modified music system

--- src/components/SideNav/Music/index.js
+++ src/components/SideNav/Music/index.js
@@ -1,23 +1,31 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Volume2, Volume, Play, Pause } from 'react-feather';
 import lofiBokuNoHero from '../../../assets/my hero academia the day lofi.mp3';
+import lofiNova from '../../../assets/mell-ø - nova.mp3';
 import { MusicArea } from './style';
 
 export default function Music(props) {
   const [muted, setMuted] = useState(false);
   const [isPlaying, setIsPlaying] = useState(true);
   const music = useRef(null);
+  useEffect(() => {
+    const randomNumber = Math.random() * 2;
+
+    if (randomNumber <= 1) {
+      music.current.src = lofiNova;
+    }
+    if (randomNumber > 1) {
+      music.current.src = lofiBokuNoHero;
+    }
+  }, []);
   const renderVideo = () => (
     <audio
       {...(muted ? { muted: true } : {})}
       onCanPlay={(e) => setIsPlaying(e.target.paused)}
-      autoPlay={false}
+      autoPlay={true}
       ref={music}
-    >
-      <source src={lofiBokuNoHero} type="audio/mpeg"></source>
-    </audio>
+    ></audio>
   );
-
   const handlePlay = () => {
     isPlaying ? music.current.play() : music.current.pause();
     setIsPlaying(!isPlaying);
