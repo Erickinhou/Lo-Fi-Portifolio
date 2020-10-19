115ff1fd0c69e18efa80f386bd1c82746c862bd9
Erickinhou <willianerick17@gmail.com>
2020-10-18 22:01:35 -0300

    created home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,70 @@
 import React from 'react';
-import { Background } from './style';
-import SideNav from '../../components/SideNav';
+import { ArrowLeft } from 'react-feather';
+import {
+  Background,
+  Text,
+  TextMain,
+  MainArea,
+  TextHighlight,
+  Button,
+  StrokeButton,
+  MusicTextArea,
+  Agency,
+  MinorText,
+  AgencyLink,
+} from './style';
+import { useState } from 'react';
 
 export default function Home() {
+  const handleButtonOnHover = (event) => {
+    if (!event.target.getAttribute('fill')) setButtonContent(!buttonContent);
+    else setButtonFillContent(!buttonFillContent);
+  };
+  const handleButtonOffHover = (event) => {
+    console.log('teste');
+    if (!event.target.getAttribute('fill')) setButtonContent(!buttonContent);
+    else setButtonFillContent(!buttonFillContent);
+    console.log(buttonContent);
+  };
+
+  const buttonsProps = {
+    onMouseOver: handleButtonOnHover,
+    onMouseLeave: handleButtonOffHover,
+  };
+  const [buttonContent, setButtonContent] = useState(false);
+  const [buttonFillContent, setButtonFillContent] = useState(false);
+
   return (
     <Background>
-      <SideNav></SideNav>
+      <MainArea>
+        <Text>Hi, my name is Erick.</Text>
+        <TextMain>
+          I'm a <TextHighlight>FullStack Developer</TextHighlight>,
+        </TextMain>
+        <Text>
+          a Brazilian who loves to create new things
+          <br /> and a listener of lofi :p
+        </Text>
+        <div>
+          <Button {...buttonsProps} to="/works">
+            {buttonContent ? 'Thanks!' : 'See my works'}
+          </Button>
+          <StrokeButton fill="true" {...buttonsProps}>
+            {buttonFillContent ? 'Thanks!' : 'About Me'}
+          </StrokeButton>
+        </div>
+      </MainArea>
+      <MusicTextArea>
+        <ArrowLeft></ArrowLeft>
+        <MinorText> Hit the play button and chill </MinorText>
+      </MusicTextArea>
+      <Agency>
+        Meet my agency in <span> </span>
+        <AgencyLink target="_blank" href="http://lecode.dev">
+          lecode
+        </AgencyLink>
+        .
+      </Agency>
     </Background>
   );
 }
--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -1,10 +1,99 @@
 import styled from 'styled-components';
 import Its1amGif from '../../assets/Its1am.gif';
+import { Link } from 'react-router-dom';
+
 const Background = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   width: 100vw;
   height: 100vh;
   background: url(${Its1amGif});
   background-size: cover;
+  font-family: 'Montserrat', sans-serif;
 `;
 
-export { Background };
+const MainArea = styled.div`
+  color: #fff;
+  margin: 4vh 0 0 8vw;
+`;
+
+const Text = styled.h3`
+  font-weight: 300;
+  font-size: 1.5rem;
+`;
+const TextMain = styled.h1`
+  font-weight: 300;
+  font-size: 3rem;
+`;
+const TextHighlight = styled.b`
+  color: var(--red);
+  font-weight: 400;
+`;
+//Buttons Area
+const Button = styled(Link)`
+  display: inline-block;
+  padding: 15px 0;
+  width: 200px;
+  text-align: center;
+  color: #fff;
+  background-color: var(--red);
+  text-decoration: none;
+  border-radius: 5px;
+  font-size: 1.5rem;
+  margin: 0 2vw 0 0;
+  transition: all 0.4s ease-in-out;
+  &:hover {
+    background-color: var(--light);
+  }
+`;
+
+const StrokeButton = styled(Button)`
+  background-color: transparent;
+  border: 2px solid white;
+  margin: 0 0 0 2vw;
+  &:hover {
+    border: 2px solid var(--light);
+  }
+`;
+
+//Agency area
+
+const Agency = styled.p`
+  position: absolute;
+  bottom: 0;
+  right: 1vw;
+  color: var(--light);
+  font-size: 1.2rem;
+  font-weight: 300;
+`;
+const AgencyLink = styled.a`
+  color: var(--blue);
+`;
+
+//Music text area
+const MusicTextArea = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 1.2rem;
+  color: var(--light);
+  margin: 0 0 0 8vw;
+`;
+
+const MinorText = styled.p`
+  margin-left: 0.5vw;
+`;
+
+export {
+  Background,
+  Text,
+  TextMain,
+  TextHighlight,
+  MainArea,
+  Button,
+  StrokeButton,
+  Agency,
+  MusicTextArea,
+  MinorText,
+  AgencyLink,
+};
