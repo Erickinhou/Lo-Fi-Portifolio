4410e42607e609296a9480b3830cd47d980e5874
Erickinhou <willianerick17@gmail.com>
2020-12-16 16:50:14 -0300

    Fix cursor pointer in the music component

--- src/components/SideNav/Music/style.js
+++ src/components/SideNav/Music/style.js
@@ -3,7 +3,10 @@ import styled from 'styled-components';
 const MusicArea = styled.div`
   display: flex;
   width: 100%;
-  justify-content: space-evenly;
+  justify-content: space-evenly;  
+  & > div{
+    cursor: pointer;
+  }
 `;
 
 export { MusicArea };
